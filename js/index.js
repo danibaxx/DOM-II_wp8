840ec5fa5893e945b242bd01d04a73cc88ec3cc6
danibaxx <danibaxx@hotmail.com>
2019-08-11 18:14:44 -0600

    added click event on navBar within index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+let navBar = document.querySelector('.main-navigation');
+  navBar.addEventListener('click', event => {
+    event.target.style.backgroundColor = '#F3FF69';
+  });
+
